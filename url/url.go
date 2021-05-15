8431674c0fd3dd299548eb57c2515959cbf20e86
Alexey Sergey <alexey.sergey@softswiss.com>
2021-05-15 15:46:30 +0300

    new url type for only http/https schema

--- url/url.go
+++ url/url.go
@@ -9,11 +9,12 @@ import (
 	"database/sql/driver"
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/asaskevich/govalidator"
 )
 
-// URL represents a URL type
+// URL represents a URL type with schema
 type URL string
 
 //Value implementation of driver.Valuer
@@ -31,7 +32,7 @@ func (url URL) Value() (value driver.Value, err error) {
 
 //Validate implementation of ozzo-validation Validate interface
 func (url URL) Validate() error {
-	if !govalidator.IsURL(url.String()) {
+	if !govalidator.IsRequestURL(url.String()) {
 		return fmt.Errorf("'%s' is not valid url", url)
 	}
 
@@ -57,5 +58,56 @@ func (url *URL) UnmarshalJSON(data []byte) error {
 
 //String implementation of Stringer interface
 func (url URL) String() string {
+	return string(url)
+}
+
+// URL represents a URL type with schema
+type HttpURL string
+
+//Value implementation of driver.Valuer
+func (url HttpURL) Value() (value driver.Value, err error) {
+	if url == "" {
+		return "", nil
+	}
+
+	if err = url.Validate(); err != nil {
+		return nil, err
+	}
+
+	return url.String(), nil
+}
+
+//Validate implementation of ozzo-validation Validate interface
+func (url HttpURL) Validate() error {
+	if !strings.HasPrefix(url.String(), "http:") || !strings.HasPrefix(url.String(), "https:") {
+		return fmt.Errorf("'%s' is not valid url schema", url)
+	}
+
+	if !govalidator.IsRequestURL(url.String()) {
+		return fmt.Errorf("'%s' is not valid url", url)
+	}
+
+	return nil
+}
+
+//UnmarshalJSON unmarshall implementation for Email
+func (url *HttpURL) UnmarshalJSON(data []byte) error {
+	var str string
+	if err := json.Unmarshal(data, &str); err != nil {
+		return err
+	}
+
+	value := HttpURL(str)
+	if err := value.Validate(); err != nil {
+		return err
+	}
+
+	*url = value
+
+	return nil
+}
+
+//String implementation of Stringer interface
+func (url HttpURL) String() string {
 	return string(url)
 }
