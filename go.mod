4017925052643946d83fc69d5cd32f0631c22ed7
asechiho <45268776+asechiho@users.noreply.github.com>
2022-05-10 14:47:16 +0300

    add custom NullTime. upgrade go -> 1.18 (#16)
    Co-authored-by: alexey.sergey <alexey.sergey@softswiss.com>

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/mikekonan/go-types
 
-go 1.14
+go 1.18
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
---
+++ time/time_test.go
@@ -0,0 +1,227 @@
+package time
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"testing"
+	"time"
+)
+
+type tm struct {
+	XMLName xml.Name `json:"-" xml:"test"`
+	T       Time     `json:"t,omitempty" xml:"t,omitempty"`
+}
+
+var unmarshalTimeTestCases = []unmarshalType{
+	{
+		testJSON:               []byte(`{}`),
+		testXML:                []byte(`<?xml version="1.0"?><test></test>`),
+		expectingValidateError: false,
+	},
+	{
+		testJSON:               []byte(`{"t": ""}`),
+		testXML:                []byte(`<?xml version="1.0"?><test><t></t></test>`),
+		expectingValidateError: false,
+	},
+	{
+		testJSON:               []byte(`{"t": "2022-05-10T12:35:10+03:00"}`),
+		testXML:                []byte(`<?xml version="1.0"?><test><t>2022-05-10T12:35:10+03:00</t></test>`),
+		expectingValidateError: false,
+	},
+	{
+		testJSON:               []byte(`{"t": 1234}`),
+		testXML:                []byte(`test><t>@</t>`),
+		expectingValidateError: true,
+	},
+	{
+		testJSON:               []byte(`{"t": "@"}`),
+		testXML:                []byte(`<?xml version="1.0"?><test><t>@</t></test>`),
+		expectingValidateError: true,
+	},
+	{
+		testJSON:               []byte(`{"t": "2006-01-02"}`),
+		testXML:                []byte(`<?xml version="1.0"?><test><t>2006-01-02</t></test>`),
+		expectingValidateError: true,
+	},
+	{
+		testJSON:               []byte(`{"t": "00:00:00"}`),
+		testXML:                []byte(`<?xml version="1.0"?><test><t>00:00:00</t></test>`),
+		expectingValidateError: true,
+	},
+	{
+		testJSON:               []byte(`{"t": "text-text-text"}`),
+		testXML:                []byte(`<?xml version="1.0"?><test><t>text-text-text</t></test>`),
+		expectingValidateError: true,
+	},
+}
+
+func TestTimeUnmarshalJSON(t *testing.T) {
+	testTimeUnmarshal(t, json.Unmarshal, func(u unmarshalType) []byte {
+		return u.testJSON
+	})
+}
+
+func TestTimeUnmarshalXML(t *testing.T) {
+	testTimeUnmarshal(t, xml.Unmarshal, func(u unmarshalType) []byte {
+		return u.testXML
+	})
+}
+
+func testTimeUnmarshal(t *testing.T, unmarshalFunc func([]byte, any) error, valueFunc func(unmarshalType) []byte) {
+	for _, testCase := range unmarshalTimeTestCases {
+		var d tm
+		actualErr := unmarshalFunc(valueFunc(testCase), &d)
+		if (actualErr == nil) == testCase.expectingValidateError {
+			t.Errorf(`Validate: '%s'. expecting error - '%v' but was opposite`, valueFunc(testCase), testCase.expectingValidateError)
+		}
+	}
+}
+
+type marshalTimeType struct {
+	testData     tm
+	expectedJSON []byte
+	expectedXML  []byte
+}
+
+var marshalTimeTestCases = []marshalTimeType{
+	{
+		testData:     tm{},
+		expectedJSON: []byte(`{"t":null}`),
+		expectedXML:  []byte(`<test></test>`),
+	},
+	{
+		testData:     tm{T: NullTime(time.Date(2006, 1, 2, 0, 0, 0, 0, time.UTC))},
+		expectedJSON: []byte(`{"t":"2006-01-02T00:00:00Z"}`),
+		expectedXML:  []byte(`<test><t>2006-01-02T00:00:00Z</t></test>`),
+	},
+	{
+		testData:     tm{T: NullTime(time.Date(1996, 8, 30, 10, 11, 40, 0, time.UTC))},
+		expectedJSON: []byte(`{"t":"1996-08-30T10:11:40Z"}`),
+		expectedXML:  []byte(`<test><t>1996-08-30T10:11:40Z</t></test>`),
+	},
+}
+
+func TestTimeMarshalJSON(t *testing.T) {
+	testTimeMarshal(t, "json", json.Marshal, func(m marshalTimeType) []byte {
+		return m.expectedJSON
+	})
+}
+
+func TestTimeMarshalXML(t *testing.T) {
+	testTimeMarshal(t, "xml", xml.Marshal, func(m marshalTimeType) []byte {
+		return m.expectedXML
+	})
+}
+
+func testTimeMarshal(t *testing.T, tp string, marshalFunc func(any) ([]byte, error), valueFunc func(timeType marshalTimeType) []byte) {
+	for _, testCase := range marshalTimeTestCases {
+		actual, actualErr := marshalFunc(testCase.testData)
+		if actualErr != nil {
+			t.Errorf(`%s`, actualErr)
+		}
+		if string(actual) != string(valueFunc(testCase)) {
+			t.Errorf("Actual %s does not equal expected. Actual: '%s', expected: '%s'", tp, actual, valueFunc(testCase))
+		}
+	}
+}
+
+func TestTimeFormatValid(t *testing.T) {
+	var (
+		d        = NullTime(time.Now())
+		expected = d.Time.Format(time.RFC3339)
+		str      = d.format()
+	)
+	if *str != expected {
+		t.Errorf("Invalid format(). Actual: %s, expected: %s", *str, expected)
+
+	}
+}
+
+func TestTimeFormatInvalid(t *testing.T) {
+	var (
+		d   = Time{}
+		str = d.format()
+	)
+	if str != nil {
+		t.Errorf("Invalid format(). Actual: %s, expected: nil", *str)
+
+	}
+}
+
+var testCaseTimeParseString = []struct {
+	value         string
+	expectedError bool
+}{
+	// not time
+	{
+		value:         "invalid-format",
+		expectedError: true,
+	},
+	// invalid format not rfc3339 w/a time
+	{
+		value:         "10-01-1996",
+		expectedError: true,
+	},
+	// date instead of date time
+	{
+		value:         "1996-09-29",
+		expectedError: true,
+	},
+	// only time instead of date time
+	{
+		value:         "00:00:00",
+		expectedError: true,
+	},
+	{
+		value:         "",
+		expectedError: false,
+	},
+	{
+		value:         "1998-02-20T12:21:40Z",
+		expectedError: false,
+	},
+}
+
+func TestParseTimeFromString(t *testing.T) {
+	for _, testCase := range testCaseTimeParseString {
+		var tm = &Time{}
+		actualErr := tm.parseTimeFromString(testCase.value)
+		if (actualErr == nil) == testCase.expectedError {
+			t.Errorf(`Expecting error - '%v' but was opposite`, testCase.expectedError)
+		}
+	}
+}
+
+var testCaseTimeString = []struct {
+	t              Time
+	expectedString string
+}{
+	{
+		t:              Time{},
+		expectedString: "",
+	},
+	{
+		t:              NullTime(time.Date(1996, 8, 30, 10, 11, 40, 0, time.UTC)),
+		expectedString: "1996-08-30T10:11:40Z",
+	},
+}
+
+func TestTimeString(t *testing.T) {
+	for _, testCase := range testCaseTimeString {
+		// empty var d = Date{}
+		var str = testCase.t.String()
+		if str != testCase.expectedString {
+			t.Errorf("Invalid String(). Actual: %s, expected: %s", str, testCase.expectedString)
+		}
+	}
+}
+
+func TestTimeValuer(t *testing.T) {
+	for _, testCase := range testCaseTimeString {
+		// empty var d = Date{}
+		var _, err = testCase.t.Value()
+		if err != nil {
+			t.Errorf("Invalid String(). Err: %v", err)
+		}
+	}
+}
