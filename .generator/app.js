fc7f619417b9a609d90cf3ed9ffed70b981408cb
Mikalai Konan <mikalai.konan@icloud.com>
2023-05-13 14:53:02 +0300

    fixes

--- .generator/app.js
+++ .generator/app.js
@@ -3,10 +3,7 @@ const glob = require('glob-promise');
 const fs = require('fs');
 
 glob.promise(__dirname + '/../*/*.yaml')
-    .then(files => {
-        console.log(files);
-        return files.map(f => fs.readFileSync(f, "utf8"))
-    })
+    .then(files => files.map(f => fs.readFileSync(f, "utf8")))
     .then(files => files.map(f => yaml.parse(f)))
     .then(files => {
         let schemasArr = files.map(f => Object.keys(f.components.schemas)
