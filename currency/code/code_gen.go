e3e8dfc517ac62da00aeec8b7efad5e1495c0654
Boris Kuzmin <kzmnbrs@icloud.com>
2024-09-24 14:50:59 +0400

    feat(currency): add XTS currency (#44)
    * feat(currency): add XTS
    
    * fix(currency): fix xts currency generation
    
    * fix(currency): fix override condition

--- currency/code/code_gen.go
+++ currency/code/code_gen.go
@@ -4,337 +4,337 @@ import "github.com/mikekonan/go-types/v2/currency"
 
 const (
 	// AFN represents 'AFN' currency code
-	AFN = currency.Code("AFN")
+    AFN = currency.Code("AFN")
 	// EUR represents 'EUR' currency code
-	EUR = currency.Code("EUR")
+    EUR = currency.Code("EUR")
 	// ALL represents 'ALL' currency code
-	ALL = currency.Code("ALL")
+    ALL = currency.Code("ALL")
 	// DZD represents 'DZD' currency code
-	DZD = currency.Code("DZD")
+    DZD = currency.Code("DZD")
 	// USD represents 'USD' currency code
-	USD = currency.Code("USD")
+    USD = currency.Code("USD")
 	// AOA represents 'AOA' currency code
-	AOA = currency.Code("AOA")
+    AOA = currency.Code("AOA")
 	// XCD represents 'XCD' currency code
-	XCD = currency.Code("XCD")
+    XCD = currency.Code("XCD")
 	// ARS represents 'ARS' currency code
-	ARS = currency.Code("ARS")
+    ARS = currency.Code("ARS")
 	// AMD represents 'AMD' currency code
-	AMD = currency.Code("AMD")
+    AMD = currency.Code("AMD")
 	// AWG represents 'AWG' currency code
-	AWG = currency.Code("AWG")
+    AWG = currency.Code("AWG")
 	// AUD represents 'AUD' currency code
-	AUD = currency.Code("AUD")
+    AUD = currency.Code("AUD")
 	// AZN represents 'AZN' currency code
-	AZN = currency.Code("AZN")
+    AZN = currency.Code("AZN")
 	// BSD represents 'BSD' currency code
-	BSD = currency.Code("BSD")
+    BSD = currency.Code("BSD")
 	// BHD represents 'BHD' currency code
-	BHD = currency.Code("BHD")
+    BHD = currency.Code("BHD")
 	// BDT represents 'BDT' currency code
-	BDT = currency.Code("BDT")
+    BDT = currency.Code("BDT")
 	// BBD represents 'BBD' currency code
-	BBD = currency.Code("BBD")
+    BBD = currency.Code("BBD")
 	// BYN represents 'BYN' currency code
-	BYN = currency.Code("BYN")
+    BYN = currency.Code("BYN")
 	// BZD represents 'BZD' currency code
-	BZD = currency.Code("BZD")
+    BZD = currency.Code("BZD")
 	// XOF represents 'XOF' currency code
-	XOF = currency.Code("XOF")
+    XOF = currency.Code("XOF")
 	// BMD represents 'BMD' currency code
-	BMD = currency.Code("BMD")
+    BMD = currency.Code("BMD")
 	// INR represents 'INR' currency code
-	INR = currency.Code("INR")
+    INR = currency.Code("INR")
 	// BTN represents 'BTN' currency code
-	BTN = currency.Code("BTN")
+    BTN = currency.Code("BTN")
 	// BOB represents 'BOB' currency code
-	BOB = currency.Code("BOB")
+    BOB = currency.Code("BOB")
 	// BOV represents 'BOV' currency code
-	BOV = currency.Code("BOV")
+    BOV = currency.Code("BOV")
 	// BAM represents 'BAM' currency code
-	BAM = currency.Code("BAM")
+    BAM = currency.Code("BAM")
 	// BWP represents 'BWP' currency code
-	BWP = currency.Code("BWP")
+    BWP = currency.Code("BWP")
 	// NOK represents 'NOK' currency code
-	NOK = currency.Code("NOK")
+    NOK = currency.Code("NOK")
 	// BRL represents 'BRL' currency code
-	BRL = currency.Code("BRL")
+    BRL = currency.Code("BRL")
 	// BND represents 'BND' currency code
-	BND = currency.Code("BND")
+    BND = currency.Code("BND")
 	// BGN represents 'BGN' currency code
-	BGN = currency.Code("BGN")
+    BGN = currency.Code("BGN")
 	// BIF represents 'BIF' currency code
-	BIF = currency.Code("BIF")
+    BIF = currency.Code("BIF")
 	// CVE represents 'CVE' currency code
-	CVE = currency.Code("CVE")
+    CVE = currency.Code("CVE")
 	// KHR represents 'KHR' currency code
-	KHR = currency.Code("KHR")
+    KHR = currency.Code("KHR")
 	// XAF represents 'XAF' currency code
-	XAF = currency.Code("XAF")
+    XAF = currency.Code("XAF")
 	// CAD represents 'CAD' currency code
-	CAD = currency.Code("CAD")
+    CAD = currency.Code("CAD")
 	// KYD represents 'KYD' currency code
-	KYD = currency.Code("KYD")
+    KYD = currency.Code("KYD")
 	// CLP represents 'CLP' currency code
-	CLP = currency.Code("CLP")
+    CLP = currency.Code("CLP")
 	// CLF represents 'CLF' currency code
-	CLF = currency.Code("CLF")
+    CLF = currency.Code("CLF")
 	// CNY represents 'CNY' currency code
-	CNY = currency.Code("CNY")
+    CNY = currency.Code("CNY")
 	// COP represents 'COP' currency code
-	COP = currency.Code("COP")
+    COP = currency.Code("COP")
 	// COU represents 'COU' currency code
-	COU = currency.Code("COU")
+    COU = currency.Code("COU")
 	// KMF represents 'KMF' currency code
-	KMF = currency.Code("KMF")
+    KMF = currency.Code("KMF")
 	// CDF represents 'CDF' currency code
-	CDF = currency.Code("CDF")
+    CDF = currency.Code("CDF")
 	// NZD represents 'NZD' currency code
-	NZD = currency.Code("NZD")
+    NZD = currency.Code("NZD")
 	// CRC represents 'CRC' currency code
-	CRC = currency.Code("CRC")
+    CRC = currency.Code("CRC")
 	// CUP represents 'CUP' currency code
-	CUP = currency.Code("CUP")
+    CUP = currency.Code("CUP")
 	// CUC represents 'CUC' currency code
-	CUC = currency.Code("CUC")
+    CUC = currency.Code("CUC")
 	// ANG represents 'ANG' currency code
-	ANG = currency.Code("ANG")
+    ANG = currency.Code("ANG")
 	// CZK represents 'CZK' currency code
-	CZK = currency.Code("CZK")
+    CZK = currency.Code("CZK")
 	// DKK represents 'DKK' currency code
-	DKK = currency.Code("DKK")
+    DKK = currency.Code("DKK")
 	// DJF represents 'DJF' currency code
-	DJF = currency.Code("DJF")
+    DJF = currency.Code("DJF")
 	// DOP represents 'DOP' currency code
-	DOP = currency.Code("DOP")
+    DOP = currency.Code("DOP")
 	// EGP represents 'EGP' currency code
-	EGP = currency.Code("EGP")
+    EGP = currency.Code("EGP")
 	// SVC represents 'SVC' currency code
-	SVC = currency.Code("SVC")
+    SVC = currency.Code("SVC")
 	// ERN represents 'ERN' currency code
-	ERN = currency.Code("ERN")
+    ERN = currency.Code("ERN")
 	// SZL represents 'SZL' currency code
-	SZL = currency.Code("SZL")
+    SZL = currency.Code("SZL")
 	// ETB represents 'ETB' currency code
-	ETB = currency.Code("ETB")
+    ETB = currency.Code("ETB")
 	// FKP represents 'FKP' currency code
-	FKP = currency.Code("FKP")
+    FKP = currency.Code("FKP")
 	// FJD represents 'FJD' currency code
-	FJD = currency.Code("FJD")
+    FJD = currency.Code("FJD")
 	// XPF represents 'XPF' currency code
-	XPF = currency.Code("XPF")
+    XPF = currency.Code("XPF")
 	// GMD represents 'GMD' currency code
-	GMD = currency.Code("GMD")
+    GMD = currency.Code("GMD")
 	// GEL represents 'GEL' currency code
-	GEL = currency.Code("GEL")
+    GEL = currency.Code("GEL")
 	// GHS represents 'GHS' currency code
-	GHS = currency.Code("GHS")
+    GHS = currency.Code("GHS")
 	// GIP represents 'GIP' currency code
-	GIP = currency.Code("GIP")
+    GIP = currency.Code("GIP")
 	// GTQ represents 'GTQ' currency code
-	GTQ = currency.Code("GTQ")
+    GTQ = currency.Code("GTQ")
 	// GBP represents 'GBP' currency code
-	GBP = currency.Code("GBP")
+    GBP = currency.Code("GBP")
 	// GNF represents 'GNF' currency code
-	GNF = currency.Code("GNF")
+    GNF = currency.Code("GNF")
 	// GYD represents 'GYD' currency code
-	GYD = currency.Code("GYD")
+    GYD = currency.Code("GYD")
 	// HTG represents 'HTG' currency code
-	HTG = currency.Code("HTG")
+    HTG = currency.Code("HTG")
 	// HNL represents 'HNL' currency code
-	HNL = currency.Code("HNL")
+    HNL = currency.Code("HNL")
 	// HKD represents 'HKD' currency code
-	HKD = currency.Code("HKD")
+    HKD = currency.Code("HKD")
 	// HUF represents 'HUF' currency code
-	HUF = currency.Code("HUF")
+    HUF = currency.Code("HUF")
 	// ISK represents 'ISK' currency code
-	ISK = currency.Code("ISK")
+    ISK = currency.Code("ISK")
 	// IDR represents 'IDR' currency code
-	IDR = currency.Code("IDR")
+    IDR = currency.Code("IDR")
 	// IRR represents 'IRR' currency code
-	IRR = currency.Code("IRR")
+    IRR = currency.Code("IRR")
 	// IQD represents 'IQD' currency code
-	IQD = currency.Code("IQD")
+    IQD = currency.Code("IQD")
 	// ILS represents 'ILS' currency code
-	ILS = currency.Code("ILS")
+    ILS = currency.Code("ILS")
 	// JMD represents 'JMD' currency code
-	JMD = currency.Code("JMD")
+    JMD = currency.Code("JMD")
 	// JPY represents 'JPY' currency code
-	JPY = currency.Code("JPY")
+    JPY = currency.Code("JPY")
 	// JOD represents 'JOD' currency code
-	JOD = currency.Code("JOD")
+    JOD = currency.Code("JOD")
 	// KZT represents 'KZT' currency code
-	KZT = currency.Code("KZT")
+    KZT = currency.Code("KZT")
 	// KES represents 'KES' currency code
-	KES = currency.Code("KES")
+    KES = currency.Code("KES")
 	// KPW represents 'KPW' currency code
-	KPW = currency.Code("KPW")
+    KPW = currency.Code("KPW")
 	// KRW represents 'KRW' currency code
-	KRW = currency.Code("KRW")
+    KRW = currency.Code("KRW")
 	// KWD represents 'KWD' currency code
-	KWD = currency.Code("KWD")
+    KWD = currency.Code("KWD")
 	// KGS represents 'KGS' currency code
-	KGS = currency.Code("KGS")
+    KGS = currency.Code("KGS")
 	// LAK represents 'LAK' currency code
-	LAK = currency.Code("LAK")
+    LAK = currency.Code("LAK")
 	// LBP represents 'LBP' currency code
-	LBP = currency.Code("LBP")
+    LBP = currency.Code("LBP")
 	// LSL represents 'LSL' currency code
-	LSL = currency.Code("LSL")
+    LSL = currency.Code("LSL")
 	// ZAR represents 'ZAR' currency code
-	ZAR = currency.Code("ZAR")
+    ZAR = currency.Code("ZAR")
 	// LRD represents 'LRD' currency code
-	LRD = currency.Code("LRD")
+    LRD = currency.Code("LRD")
 	// LYD represents 'LYD' currency code
-	LYD = currency.Code("LYD")
+    LYD = currency.Code("LYD")
 	// CHF represents 'CHF' currency code
-	CHF = currency.Code("CHF")
+    CHF = currency.Code("CHF")
 	// MOP represents 'MOP' currency code
-	MOP = currency.Code("MOP")
+    MOP = currency.Code("MOP")
 	// MKD represents 'MKD' currency code
-	MKD = currency.Code("MKD")
+    MKD = currency.Code("MKD")
 	// MGA represents 'MGA' currency code
-	MGA = currency.Code("MGA")
+    MGA = currency.Code("MGA")
 	// MWK represents 'MWK' currency code
-	MWK = currency.Code("MWK")
+    MWK = currency.Code("MWK")
 	// MYR represents 'MYR' currency code
-	MYR = currency.Code("MYR")
+    MYR = currency.Code("MYR")
 	// MVR represents 'MVR' currency code
-	MVR = currency.Code("MVR")
+    MVR = currency.Code("MVR")
 	// MRU represents 'MRU' currency code
-	MRU = currency.Code("MRU")
+    MRU = currency.Code("MRU")
 	// MUR represents 'MUR' currency code
-	MUR = currency.Code("MUR")
+    MUR = currency.Code("MUR")
 	// MXN represents 'MXN' currency code
-	MXN = currency.Code("MXN")
+    MXN = currency.Code("MXN")
 	// MXV represents 'MXV' currency code
-	MXV = currency.Code("MXV")
+    MXV = currency.Code("MXV")
 	// MDL represents 'MDL' currency code
-	MDL = currency.Code("MDL")
+    MDL = currency.Code("MDL")
 	// MNT represents 'MNT' currency code
-	MNT = currency.Code("MNT")
+    MNT = currency.Code("MNT")
 	// MAD represents 'MAD' currency code
-	MAD = currency.Code("MAD")
+    MAD = currency.Code("MAD")
 	// MZN represents 'MZN' currency code
-	MZN = currency.Code("MZN")
+    MZN = currency.Code("MZN")
 	// MMK represents 'MMK' currency code
-	MMK = currency.Code("MMK")
+    MMK = currency.Code("MMK")
 	// NAD represents 'NAD' currency code
-	NAD = currency.Code("NAD")
+    NAD = currency.Code("NAD")
 	// NPR represents 'NPR' currency code
-	NPR = currency.Code("NPR")
+    NPR = currency.Code("NPR")
 	// NIO represents 'NIO' currency code
-	NIO = currency.Code("NIO")
+    NIO = currency.Code("NIO")
 	// NGN represents 'NGN' currency code
-	NGN = currency.Code("NGN")
+    NGN = currency.Code("NGN")
 	// OMR represents 'OMR' currency code
-	OMR = currency.Code("OMR")
+    OMR = currency.Code("OMR")
 	// PKR represents 'PKR' currency code
-	PKR = currency.Code("PKR")
+    PKR = currency.Code("PKR")
 	// PAB represents 'PAB' currency code
-	PAB = currency.Code("PAB")
+    PAB = currency.Code("PAB")
 	// PGK represents 'PGK' currency code
-	PGK = currency.Code("PGK")
+    PGK = currency.Code("PGK")
 	// PYG represents 'PYG' currency code
-	PYG = currency.Code("PYG")
+    PYG = currency.Code("PYG")
 	// PEN represents 'PEN' currency code
-	PEN = currency.Code("PEN")
+    PEN = currency.Code("PEN")
 	// PHP represents 'PHP' currency code
-	PHP = currency.Code("PHP")
+    PHP = currency.Code("PHP")
 	// PLN represents 'PLN' currency code
-	PLN = currency.Code("PLN")
+    PLN = currency.Code("PLN")
 	// QAR represents 'QAR' currency code
-	QAR = currency.Code("QAR")
+    QAR = currency.Code("QAR")
 	// RON represents 'RON' currency code
-	RON = currency.Code("RON")
+    RON = currency.Code("RON")
 	// RUB represents 'RUB' currency code
-	RUB = currency.Code("RUB")
+    RUB = currency.Code("RUB")
 	// RWF represents 'RWF' currency code
-	RWF = currency.Code("RWF")
+    RWF = currency.Code("RWF")
 	// SHP represents 'SHP' currency code
-	SHP = currency.Code("SHP")
+    SHP = currency.Code("SHP")
 	// WST represents 'WST' currency code
-	WST = currency.Code("WST")
+    WST = currency.Code("WST")
 	// STN represents 'STN' currency code
-	STN = currency.Code("STN")
+    STN = currency.Code("STN")
 	// SAR represents 'SAR' currency code
-	SAR = currency.Code("SAR")
+    SAR = currency.Code("SAR")
 	// RSD represents 'RSD' currency code
-	RSD = currency.Code("RSD")
+    RSD = currency.Code("RSD")
 	// SCR represents 'SCR' currency code
-	SCR = currency.Code("SCR")
-	// SLL represents 'SLL' currency code
-	SLL = currency.Code("SLL")
+    SCR = currency.Code("SCR")
 	// SLE represents 'SLE' currency code
-	SLE = currency.Code("SLE")
+    SLE = currency.Code("SLE")
 	// SGD represents 'SGD' currency code
-	SGD = currency.Code("SGD")
+    SGD = currency.Code("SGD")
 	// SBD represents 'SBD' currency code
-	SBD = currency.Code("SBD")
+    SBD = currency.Code("SBD")
 	// SOS represents 'SOS' currency code
-	SOS = currency.Code("SOS")
+    SOS = currency.Code("SOS")
 	// SSP represents 'SSP' currency code
-	SSP = currency.Code("SSP")
+    SSP = currency.Code("SSP")
 	// LKR represents 'LKR' currency code
-	LKR = currency.Code("LKR")
+    LKR = currency.Code("LKR")
 	// SDG represents 'SDG' currency code
-	SDG = currency.Code("SDG")
+    SDG = currency.Code("SDG")
 	// SRD represents 'SRD' currency code
-	SRD = currency.Code("SRD")
+    SRD = currency.Code("SRD")
 	// SEK represents 'SEK' currency code
-	SEK = currency.Code("SEK")
+    SEK = currency.Code("SEK")
 	// CHE represents 'CHE' currency code
-	CHE = currency.Code("CHE")
+    CHE = currency.Code("CHE")
 	// CHW represents 'CHW' currency code
-	CHW = currency.Code("CHW")
+    CHW = currency.Code("CHW")
 	// SYP represents 'SYP' currency code
-	SYP = currency.Code("SYP")
+    SYP = currency.Code("SYP")
 	// TWD represents 'TWD' currency code
-	TWD = currency.Code("TWD")
+    TWD = currency.Code("TWD")
 	// TJS represents 'TJS' currency code
-	TJS = currency.Code("TJS")
+    TJS = currency.Code("TJS")
 	// TZS represents 'TZS' currency code
-	TZS = currency.Code("TZS")
+    TZS = currency.Code("TZS")
 	// THB represents 'THB' currency code
-	THB = currency.Code("THB")
+    THB = currency.Code("THB")
 	// TOP represents 'TOP' currency code
-	TOP = currency.Code("TOP")
+    TOP = currency.Code("TOP")
 	// TTD represents 'TTD' currency code
-	TTD = currency.Code("TTD")
+    TTD = currency.Code("TTD")
 	// TND represents 'TND' currency code
-	TND = currency.Code("TND")
+    TND = currency.Code("TND")
 	// TRY represents 'TRY' currency code
-	TRY = currency.Code("TRY")
+    TRY = currency.Code("TRY")
 	// TMT represents 'TMT' currency code
-	TMT = currency.Code("TMT")
+    TMT = currency.Code("TMT")
 	// UGX represents 'UGX' currency code
-	UGX = currency.Code("UGX")
+    UGX = currency.Code("UGX")
 	// UAH represents 'UAH' currency code
-	UAH = currency.Code("UAH")
+    UAH = currency.Code("UAH")
 	// AED represents 'AED' currency code
-	AED = currency.Code("AED")
+    AED = currency.Code("AED")
 	// USN represents 'USN' currency code
-	USN = currency.Code("USN")
+    USN = currency.Code("USN")
 	// UYU represents 'UYU' currency code
-	UYU = currency.Code("UYU")
+    UYU = currency.Code("UYU")
 	// UYI represents 'UYI' currency code
-	UYI = currency.Code("UYI")
+    UYI = currency.Code("UYI")
 	// UYW represents 'UYW' currency code
-	UYW = currency.Code("UYW")
+    UYW = currency.Code("UYW")
 	// UZS represents 'UZS' currency code
-	UZS = currency.Code("UZS")
+    UZS = currency.Code("UZS")
 	// VUV represents 'VUV' currency code
-	VUV = currency.Code("VUV")
+    VUV = currency.Code("VUV")
 	// VES represents 'VES' currency code
-	VES = currency.Code("VES")
+    VES = currency.Code("VES")
 	// VED represents 'VED' currency code
-	VED = currency.Code("VED")
+    VED = currency.Code("VED")
 	// VND represents 'VND' currency code
-	VND = currency.Code("VND")
+    VND = currency.Code("VND")
 	// YER represents 'YER' currency code
-	YER = currency.Code("YER")
+    YER = currency.Code("YER")
 	// ZMW represents 'ZMW' currency code
-	ZMW = currency.Code("ZMW")
-	// ZWL represents 'ZWL' currency code
-	ZWL = currency.Code("ZWL")
+    ZMW = currency.Code("ZMW")
+	// ZWG represents 'ZWG' currency code
+    ZWG = currency.Code("ZWG")
+	// XTS represents 'XTS' currency code
+    XTS = currency.Code("XTS")
 )
