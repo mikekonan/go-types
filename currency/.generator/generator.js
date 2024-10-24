b3fe36d90f5c0d2404257aaa5900f0d879d7cecc
frvade <1924461+frvade@users.noreply.github.com>
2024-10-24 16:40:29 +0300

    Historical currencies feature (#46)
    * Added parsing of historical currencies
    Added isHistorical field to currency model
    
    * explicit vars
    
    * endline, go fmt

--- currency/.generator/generator.js
+++ currency/.generator/generator.js
@@ -10,185 +10,105 @@ const currencyOverrides = {
     }
 }
 
-let resp = request(
+let actualCurrencies = request(
     "GET",
     "https://www.six-group.com/dam/download/financial-information/data-center/iso-currrency/lists/list-one.xml"
 );
 
-if (resp.statusCode !== 200) {
-    console.log(`unexpected status code - '${resp.statusCode}'`);
+let historicalCurrencies = request(
+    "GET",
+    "https://www.six-group.com/dam/download/financial-information/data-center/iso-currrency/lists/list-three.xml"
+);
+
+if (actualCurrencies.statusCode !== 200 || historicalCurrencies.statusCode !== 200) {
+    console.log(`unexpected status code - actual currencies: '${actualCurrencies.statusCode}', historical: ${historicalCurrencies.statusCode}`);
     os.exit(1);
 }
 
-let currencyCodes = (currencies) => {
-    let temp = {}
-    currencies.forEach((c) => {
-        if (!!!temp[c.code]) {
-            temp[c.code] = {
-                code: c.code,
-            };
+const buildCurrency = (c) => (
+    {
+        countries: [c.country],
+        currency: c.currency,
+        code: c.code,
+        number: c.number,
+        decimalPlaces: c.decimalPlaces,
+        isHistorical: c.isHistorical
+    }
+)
+const currenciesMapWithCountries = (currencies, keyField, multi = false) => 
+    currencies.reduce((acc, c) => {
+        if (multi) {
+            acc[c[keyField]] = [...(acc[c[keyField]] || []), buildCurrency(c)]
+        } else {
+            acc[c[keyField]] && acc[c[keyField]].countries.push(c.country) || (acc[c[keyField]] = buildCurrency(c))
         }
-    })
+        return acc
+    }, {})
+const currenciesByCodeMap = (currencies) => currenciesMapWithCountries(currencies, 'code')
+const currenciesByNumberMap = (currencies) => currenciesMapWithCountries(currencies, 'number')
+const currenciesByCurrencyMap = (currencies) => currenciesMapWithCountries(currencies, 'currency')
+const currenciesByCountryMap = (currencies) => currenciesMapWithCountries(currencies, 'country', true)
 
-    return temp
-}
+const currencyCodes = (currencies) => Object.keys(currenciesByCodeMap(currencies))
+const renderCodes = (currenciesCodes) => 
+    currenciesCodes.map(
+        (code) => `\t// ${code} represents '${code}' currency code
+    ${code} = currency.Code("${code}")`
+    ).join("\n")
 
-let currenciesByCodeMap = (currencies) => {
-    let temp = {}
-    currencies.forEach((c) => {
-        if (!!!temp[c.code]) {
-            temp[c.code] = {
-                countries: [c.country],
-                currency: c.currency,
-                code: c.code,
-                number: c.number,
-                decimalPlaces: c.decimalPlaces,
-            };
+const renderCurrency = (key, currency, countries) =>
+`${key ? `\`${key}\`: ` : ''}{
+  countries:     Countries{${[...new Set(countries)].map((country) => `\`${country}\``).join(", ")}},
+  currency:      \`${currency.currency}\`,
+  code:          \`${currency.code}\`,
+  number:        \`${currency.number}\`,
+  decimalPlaces: ${currency.decimalPlaces},
+  isHistorical:  ${currency.isHistorical ? 'true' : 'false'},
+}`
+const renderCurrenciesByKey = (currenciesMap) => 
+    Object.entries(currenciesMap).map(([key, c]) => renderCurrency(key, c, c.countries))
+    .join(`,\n`)
+const renderCurrenciesByCountry = (currenciesByCountryMap, currenciesByCodeMap) =>
+    Object.entries(currenciesByCountryMap).map(([countryCode, countries]) =>
+`\`${countryCode}\`: {
+    ${countries.map((c) => renderCurrency(null, c, currenciesByCodeMap[c.code].countries)).join(`, `)},
+}`)
+    .join(`, `)
 
-            return;
-        }
-
-        temp[c.code].countries.push(c.country);
-    });
-
-    return temp
-}
-
-let currenciesByNumberMap = (currencies) => {
-    let temp = {}
-    currencies.forEach((c) => {
-        if (!!!temp[c.number]) {
-            temp[c.number] = {
-                countries: [c.country],
-                currency: c.currency,
-                code: c.code,
-                number: c.number,
-                decimalPlaces: c.decimalPlaces,
-            };
-
-            return;
-        }
-
-        temp[c.number].countries.push(c.country);
-    });
-
-    return temp
-}
-
-let currenciesByCountryMap = (currencies) => {
-    let temp = {}
-    currencies.forEach((c) => {
-        if (!!temp[c.country]) {
-            temp[c.country].currencies.push(c);
-            return;
-        }
-
-        temp[c.country] = {
-            currencies: [c],
-        };
-
-        return;
-    });
-
-    return temp
-}
-
-let currenciesByCurrencyMap = (currencies) => {
-    let temp = {}
-    currencies.forEach((c) => {
-        if (!!!temp[c.currency]) {
-            temp[c.currency] = {
-                countries: [c.country],
-                currency: c.currency,
-                code: c.code,
-                number: c.number,
-                decimalPlaces: c.decimalPlaces,
-            };
-
-            return;
-        }
-
-        temp[c.currency].countries.push(c.country);
-    });
-
-    return temp
-}
-
-let renderByCountry = (currenciesMap, currenciesByCodeMap) => {
-    return Object.keys(currenciesMap).map(
-        (key) => `\`${key}\`: {
-          ${currenciesMap[key].currencies.map((c) => {
-        return `{
-              countries:   Countries{${currenciesByCodeMap[c.code].countries
-              .map((country) => `\`${country}\``).join(", ")}},
-              currency:    \`${c.currency}\`,
-              code:        \`${c.code}\`,
-              number:      \`${c.number}\`,
-              decimalPlaces: ${c.decimalPlaces},
-          }`;
-        })
-        .join(`,`)},
-    }`
-        )
-        .join(`,`);
-};
-
-let renderCodes = (currenciesMap) => {
-    return Object.keys(currenciesMap).map(
-        (key) => `\t// ${key} represents '${key}' currency code
-    ${key} = currency.Code("${key}")`
-    ).join("\n");
-};
-
-let render = (currenciesMap) => {
-    return Object.keys(currenciesMap).map(
-        (key) => `\`${key}\`: {
-      countries:  Countries{${currenciesMap[key].countries
-            .map((country) => `\`${country}\``)
-            .join(", ")}},
-      currency:     \`${currenciesMap[key].currency}\`,
-      code:         \`${currenciesMap[key].code}\`,
-      number:       \`${currenciesMap[key].number}\`,
-      decimalPlaces: ${currenciesMap[key].decimalPlaces},
-    }`
-    ).join(`,
-    `);
-};
-
-let renderCurrencyCodesTemplate = (currencies) => {
+const renderCurrencyCodesTemplate = (currencies) => {
     return `package code
 
 import "github.com/mikekonan/go-types/v2/currency"
 
 const (
 ${renderCodes(currencyCodes(currencies))}
-)`
+)
+`
 };
 
-let renderCurrencyMappingTemplate = (currencies) => {
+const renderCurrencyMappingTemplate = (currencies) => {
     return `package currency
 
 var currenciesByCode = map[string]currency{
-  ${render(currenciesByCodeMap(currencies))},
+  ${renderCurrenciesByKey(currenciesByCodeMap(currencies))},
 }
 
 var currenciesByNumber = map[string]currency{
-  ${render(currenciesByNumberMap(currencies))},
+  ${renderCurrenciesByKey(currenciesByNumberMap(currencies))},
 }
 
 var currenciesByCountry = map[string]currencies{
-  ${renderByCountry(currenciesByCountryMap(currencies), currenciesByCodeMap(currencies))},
+  ${renderCurrenciesByCountry(currenciesByCountryMap(currencies), currenciesByCodeMap(currencies))},
 }
 
 var currenciesByCurrency = map[string]currency{
-  ${render(currenciesByCurrencyMap(currencies))},
-}`;
+  ${renderCurrenciesByKey(currenciesByCurrencyMap(currencies))},
+}
+`;
 };
 
-let normalizeCurrency = (currency) => {
-    let normalizeCountry;
-
-    normalizeCountry = (country) => {
+const normalizeCurrency = (currency) => {
+    const normalizeCountry = (country) => {
         let bracketsFound = false;
 
         if (country.includes(" (") && country.includes(")")) {
@@ -211,10 +131,10 @@ let normalizeCurrency = (currency) => {
             }
         }
 
-        return {country: country, bracketsFound: bracketsFound};
+        return { country: country, bracketsFound: bracketsFound };
     };
 
-    let normalizedResult = normalizeCountry(currency.country);
+    const normalizedResult = normalizeCountry(currency.country);
     if (normalizedResult.bracketsFound) {
         return {
             country: normalizedResult.country,
@@ -222,80 +142,92 @@ let normalizeCurrency = (currency) => {
             code: currency.code,
             number: currency.number,
             decimalPlaces: currency.decimalPlaces,
+            isHistorical: currency.isHistorical,
         };
     }
+}
 
-    return null;
-};
-
-let goCodePromise = xml2js
-    .parseStringPromise(resp.body.toString(), {mergeAttrs: true})
-    .then((result) => {
-        return Promise.resolve(result["ISO_4217"]["CcyTbl"][0]["CcyNtry"]);
-    })
-    .catch((err) => {
-        console.log(err);
-        os.exit(1);
-    })
-    .then((currencies) => {
-        let result = currencies.map((row) => {
-            if (!!!row["Ccy"]) {
-                return;
+const parseCurrenciesFromXML = (rows, useOverride = true, isHistorical = false) => {
+    return rows
+        .map((row) => {
+            if (!row["Ccy"]) {
+                return
             }
 
             const currencyCode = row["Ccy"][0]
-            const override = currencyOverrides[currencyCode]
+            const override = useOverride && !isHistorical && currencyOverrides[currencyCode]
 
-            if (row["CcyMnrUnts"][0] === "N.A." && (!override || !override.decimalPlaces)) {
-                return;
+            if (row["CcyMnrUnts"] && row["CcyMnrUnts"][0] === "N.A." && (!override || !override.decimalPlaces)) {
+                return
             }
 
             if (row["CcyNm"][0] === "No universal currency" && (!override || !override.country)) {
-                return;
+                return
             }
 
-            let parsedCurrency = {
+            const parsedCurrency = {
                 country: row["CtryNm"][0],
                 currency:
                     typeof row["CcyNm"][0] !== "object"
                         ? row["CcyNm"][0]
                         : row["CcyNm"][0]["_"],
                 code: row["Ccy"][0],
-                number: row["CcyNbr"][0],
-                decimalPlaces: isNaN(row["CcyMnrUnts"]) ? 0 : parseInt(row["CcyMnrUnts"]),
-            };
+                number: row["CcyNbr"] && row["CcyNbr"][0] || 0,
+                decimalPlaces: row["CcyMnrUnts"] && row["CcyMnrUnts"][0] || 0,
+                isHistorical: isHistorical
+            }
 
             if (override && typeof override === "object") {
                 Object.entries(override).forEach(([key, value]) => {
-                  parsedCurrency[key] = value;
+                    parsedCurrency[key] = value;
                 })
             }
 
             return parsedCurrency
-        });
+        })
+        .filter((r) => !!r)
+        .filter((r) => !r.country.startsWith("ZZ"))
+}
 
-        result = result
-            .filter((r) => !!r)
-            .filter((r) => !r.country.startsWith("ZZ"));
+const parseXMLResponse = (response, isHistorical = false) =>
+    xml2js
+    .parseStringPromise(response, { mergeAttrs: true })
+    .then(result => {
+        const firstKey = isHistorical ? "HstrcCcyTbl" : "CcyTbl"
+        const secondKey = isHistorical ? "HstrcCcyNtry" : "CcyNtry"
+        return Promise.resolve(result["ISO_4217"][firstKey][0][secondKey])
+    })
+    .catch(err => {
+        console.log(err)
+        os.exit(1);
+    })
 
-        let normalizedCountries = [];
+let goCodePromise = 
+    parseXMLResponse(actualCurrencies.body.toString())
+    .then(actual => {
+        return parseXMLResponse(historicalCurrencies.body.toString(), true)
+            .then(historical => parseCurrenciesFromXML(historical, false, true))
+            .then(historical => parseCurrenciesFromXML(actual).concat(historical))
+    })
+    .then((parsedCurrencies) => {
+        let normalizedCountries = []
 
-        result.forEach((currency) => {
-            let normalized = normalizeCurrency(currency);
+        parsedCurrencies.forEach((currency) => {
+            let normalized = normalizeCurrency(currency)
             if (!!normalized) {
-                normalizedCountries.push(normalized);
+                normalizedCountries.push(normalized)
             }
         });
 
-        return result.concat(normalizedCountries);
+        return parsedCurrencies.concat(normalizedCountries)
     })
     .then((currencies) => {
-            fs.writeFileSync("../code/code_gen.go", renderCurrencyCodesTemplate(currencies))
-            fs.writeFileSync("../currencies_mapping_gen.go", renderCurrencyMappingTemplate(currencies))
+        fs.writeFileSync("../code/code_gen.go", renderCurrencyCodesTemplate(currencies))
+        fs.writeFileSync("../currencies_mapping_gen.go", renderCurrencyMappingTemplate(currencies))
     });
 
 let oas3Promise = xml2js
-    .parseStringPromise(resp.body.toString(), {mergeAttrs: true})
+    .parseStringPromise(actualCurrencies.body.toString(), {mergeAttrs: true})
     .then((result) => {
         return Promise.resolve(result["ISO_4217"]["CcyTbl"][0]["CcyNtry"]);
     })
@@ -304,32 +236,7 @@ let oas3Promise = xml2js
         os.exit(1);
     })
     .then((currencies) => {
-        let result = currencies.map((row) => {
-            if (!!!row["Ccy"]) {
-                return;
-            }
-
-            if (row["CcyMnrUnts"][0] === "N.A.") {
-                return;
-            }
-
-            if (row["CcyNm"][0] === "No universal currency") {
-                return;
-            }
-
-            return {
-                country: row["CtryNm"][0],
-                currency:
-                    typeof row["CcyNm"][0] !== "object"
-                        ? row["CcyNm"][0]
-                        : row["CcyNm"][0]["_"],
-                code: row["Ccy"][0],
-                number: row["CcyNbr"][0],
-            };
-        });
-        result = result
-            .filter((r) => !!r)
-            .filter((r) => !r.country.startsWith("ZZ"));
+        let parsedCurrencies = parseCurrenciesFromXML(currencies, useOverride = false)
 
         const spec = {
             openapi: "3.0.0",
@@ -341,19 +248,19 @@ let oas3Promise = xml2js
                         minLength: 3,
                         maxLength: 3,
                         format: "iso4217-currency-code",
-                        enum: [...new Set(result.map((currency) => currency.code))],
+                        enum: [...new Set(parsedCurrencies.map((currency) => currency.code))],
                         "x-go-type": "github.com/mikekonan/go-types/v2/currency.Code",
                     },
                     CurrencyName: {
                         example: "Euro",
                         type: "string",
-                        enum: [...new Set(result.map((currency) => currency.currency))],
+                        enum: [...new Set(parsedCurrencies.map((currency) => currency.currency))],
                         "x-go-type": "github.com/mikekonan/go-types/v2/currency.Currency",
                     },
                     CurrencyCountry: {
                         example: "PUERTO RICO",
                         type: "string",
-                        enum: [...new Set(result.map((currency) => currency.country))],
+                        enum: [...new Set(parsedCurrencies.map((currency) => currency.country))],
                         "x-go-type": "github.com/mikekonan/go-types/v2/currency.Country",
                     },
                     CurrencyNumber: {
@@ -361,7 +268,7 @@ let oas3Promise = xml2js
                         type: "string",
                         minLength: 3,
                         maxLength: 3,
-                        enum: [...new Set(result.map((currency) => currency.number))],
+                        enum: [...new Set(parsedCurrencies.map((currency) => currency.number))],
                     },
                 },
             },
